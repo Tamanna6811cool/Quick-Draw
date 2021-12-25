b2b2d9ddb3ceef4cb9d53cf7e9b752f1d9595735
Tamanna6811cool <75354187+Tamanna6811cool@users.noreply.github.com>
2021-12-25 16:46:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup(){
+    canvas=createCanvas(250,250);
+    canvas.position(548,210);
+    background('white');
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+}
+function preload(){
+    classifier=ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+}
+function gotResult(error,results){
+if (error) {
+    console.error(error);
+}
+console.log(results);
+document.getElementById('label').innerHTML='Label:'+results[0].label;
+document.getElementById('confidence').innerHTML='Confidence:'+Math.round(results[0].confidence*100)+'%';
+utterthis= new SpeechSynthesisUtterance(results[0].label);
+synth.speak(utterthis);
+}
+function clearcanvas(){
+    background("white");
+}
